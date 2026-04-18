dd7631d93cea25f04a8e1d87f05536c875b09705
pulipakaa24 <adipu@utexas.edu>
2026-04-18 13:59:20 -0400

    realStepper

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "led_strip.h"
-#include "led_strip_rmt.h"
+#include "stepper.h"
 
 #define LED_GPIO  38  // v1.0 = 48, v1.1 = 38
 
+TaskHandle_t *poop;
+
 void app_main(void) {
-    
+    initStepper();
+    xTaskCreate(stepperTask, "stepper", 1024, NULL, 5, poop);
 }
